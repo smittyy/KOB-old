94ba73f75583f6127a5447238a16cb744ec48d27
smittyy <57191850+smittyy@users.noreply.github.com>
2020-12-15 13:31:24 -0500

    great

--- Kobold/MainWindow.xaml.cs
+++ Kobold/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace Kobold
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string UserInput;
+        string CurrentNext;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +32,31 @@ namespace Kobold
         public void HelloKOBold()
         {
             xTextAdventureOutput.Text = "Greetings, my name is Kind Operating Bot. You may also Call me KOBold. This of course has to do with my dungeon RPG style text game. Please Use these Key words to begin. Start: to start an adventure, Kill: to end this program, Josh: for the funky magic that I can create.";
+        }
+
+        private void xSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            UserInput = xTextAdventureUserImput.Text.ToLower();
+            NewCallAll();
+        }
+
+        public void NewCallAll()
+        {
+            if (UserInput == "start")
+            {
+                MakingAPTA();
+            }
+        }
+
+        public void MakingAPTA()
+        {
+            xTextAdventureOutput.Text = "Alright So Im going to generate you a new text adventure with 2 events in that adventure. For now it may be a little buggy";
+            CurrentNext = "This is the next thing to write";
+        }
+
+        private void xNext_Click(object sender, RoutedEventArgs e)
+        {
+            xTextAdventureOutput.Text = CurrentNext;
         }
     }
 }
